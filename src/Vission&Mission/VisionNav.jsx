3589d2776bd640088fc6b137b22daef412db2830
Ayaz Khan <mehmedhan7685@gmail.com>
2026-01-06 12:29:44 +0500

    Changes

--- src/Vission&Mission/VisionNav.jsx
+++ src/Vission&Mission/VisionNav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 
 const VisionNav = () => {
   const [open, setOpen] = useState(false);
