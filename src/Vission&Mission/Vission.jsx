d04fd2eb2e0e8ecf9b9691a1d8f1568c18aab355
Ayaz Khan <mehmedhan7685@gmail.com>
2026-01-06 12:17:41 +0500

    first commit

---
+++ src/Vission&Mission/Vission.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import VisionNav from './VisionNav'
+import VisionSection2 from './VisionSection2'
+import Footer from '../Footer/Footer'
+
+const Vission = () => {
+  return (
+    <div>
+      <VisionNav/>
+      <VisionSection2/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default Vission
+
